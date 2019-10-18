a7e04d00c9de4db4adc9bca7f2bb05583c730480
Leon Foks <nfoks@usgs.gov>
2019-10-17 20:54:55 -0600

    Moved cmakes to submodule

--- cmake/FindZlib.cmake
+++
@@ -1,9 +0,0 @@
-find_library(ZLIB_LIBRARY
-  NAMES zlib.a zlibstatic z libz zlib 
-  HINTS ${ZLIB_LIB_DIR} /usr "c:/MinGW64" "c:/Program Files"
-  PATH_SUFFIXES lib/x86_64-linux-gnu/ local/lib/ local/lib64 x86_64-w64-mingw32/lib "NetCDF"
-  DOC "zlib library")
-
-if( ZLIB_LIBRARY )
-  add_library(ZLIB_LIBRARY UNKNOWN IMPORTED)
-endif()
--- cmake/zlib.cmake
+++
@@ -1,9 +0,0 @@
-if ("$ENV{ZLIB_ROOT}" STREQUAL "")
-    message(FATAL_ERROR "Could not find zlib library.  Please create an environment variable ZLIB_ROOT.")
-else()
-    set(ZLIB_ROOT "$ENV{ZLIB_ROOT}" CACHE INTERNAL "Get the zlib install directory")
-    message("Using ZLIB library installed at ${ZLIB_ROOT}")
-
-    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -I${ZLIB_ROOT}/include")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L${ZLIB_ROOT}/lib -llibz")
-endif()
